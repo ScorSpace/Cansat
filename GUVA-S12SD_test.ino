95850f1d3b5c92e7f534944b44d1d2da68b53d8d
ScorSpace <76787104+ScorSpace@users.noreply.github.com>
2021-03-16 23:44:35 +0100

    Update and rename VEML6070_test.ino to GUVA-S12SD_test.ino

--- GUVA-S12SD_test.ino
+++ GUVA-S12SD_test.ino
@@ -1,6 +1,8 @@
 int pinUV = A0;
 int ValorUV;
 
+// NOTA: Mirar tabla de indices en el canal sensor-guva-s12sd en discord
+
 void setup()
 {
   pinMode(pinUV, INPUT);
@@ -10,8 +12,7 @@ void setup()
 void loop()
 {
    ValorUV = analogRead(pinUV);
-  Serial.print("Nivel de UV: ");
-  Serial.print(ValorUV);
-  Serial.println(" nm");
+  Serial.print("Índice de UV: ");
+  Serial.println(ValorUV);
   delay(1000);
 }
