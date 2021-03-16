9e04bace72a3fb0ba238edccf6ccffb073f0a640
ScorSpace <76787104+ScorSpace@users.noreply.github.com>
2021-03-17 00:37:13 +0100

    Update GPS_test.ino

--- GPS_test.ino
+++ GPS_test.ino
@@ -2,9 +2,9 @@
 
 #include <TinyGPS.h>
 
-/* This sample code demonstrates the normal use of a TinyGPS object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+/* Este código de muestra demuestra el uso normal de un objeto TinyGPS.
+   Requiere el uso de SoftwareSerial y asume que tiene un
+   dispositivo GPS serie de 4800 baudio conectado en los pines 4 (rx) y 3 (tx).
 */
 
 TinyGPS gps;
