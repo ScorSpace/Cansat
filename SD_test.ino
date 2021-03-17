e6c4cdbc0d569fd5bb4ae2000dd147fda5fac228
ScorSpace <76787104+ScorSpace@users.noreply.github.com>
2021-03-17 19:20:46 +0100

    Update SD_test.ino

--- SD_test.ino
+++ SD_test.ino
@@ -1,48 +1,48 @@
 /******************************************************************************************************/
-// Funcion:	 Mediante un modulo lector de memoria MicroSD conectado mediante interfaz SPI
+// Funcion:   Mediante un modulo lector de memoria MicroSD conectado mediante interfaz SPI
 //           crearemos un archivo de texto de prueba y grabaremos una linea en el mismo, 
-//	         luego haremos la apertura para su lectura y asi verificar el correcto funcionamiemto.
+//           luego haremos la apertura para su lectura y asi verificar el correcto funcionamiemto.
 /******************************************************************************************************/
 
-#include <SPI.h>		// incluye libreria interfaz SPI
-#include <SD.h>			// incluye libreria para tarjetas SD
+#include <SPI.h>    // incluye libreria interfaz SPI
+#include <SD.h>     // incluye libreria para tarjetas SD
 
-#define SSpin 10		// Slave Select en pin digital 10
+#define SSpin 10    // Slave Select en pin digital 10
 
-File archivo;			// objeto archivo del tipo File
+File archivo;     // objeto archivo del tipo File
 
 void setup() {
-  Serial.begin(9600);				// inicializa monitor serie a 9600 bps
-  Serial.println("Inicializando tarjeta ...");	// texto en ventana de monitor
-  if (!SD.begin(SSpin)) {			// inicializacion de tarjeta SD
+  Serial.begin(9600);       // inicializa monitor serie a 9600 bps
+  Serial.println("Inicializando tarjeta ...");  // texto en ventana de monitor
+  if (!SD.begin(SSpin)) {     // inicializacion de tarjeta SD
     Serial.println("fallo en inicializacion !");// si falla se muestra texto correspondiente y
-    return;					// se sale del setup() para finalizar el programa
+    return;         // se sale del setup() para finalizar el programa
   }
   
-  Serial.println("inicializacion correcta");	// texto de inicializacion correcta
-  archivo = SD.open("prueba.txt", FILE_WRITE);	// apertura para lectura/escritura de archivo prueba.txt
+  Serial.println("inicializacion correcta");  // texto de inicializacion correcta
+  archivo = SD.open("prueba.txt", FILE_WRITE);  // apertura para lectura/escritura de archivo prueba.txt
 
   if (archivo) {
-    archivo.println("Probando 1, 2, 3");	// escritura de una linea de texto en archivo
-    Serial.println("Escribiendo en archivo prueba.txt...");	// texto en monitor serie
-    archivo.close();				// cierre del archivo
-    Serial.println("escritura correcta");	// texto de escritura correcta en monitor serie
+    archivo.println("Probando 1, 2, 3");  // escritura de una linea de texto en archivo
+    Serial.println("Escribiendo en archivo prueba.txt..."); // texto en monitor serie
+    archivo.close();        // cierre del archivo
+    Serial.println("escritura correcta"); // texto de escritura correcta en monitor serie
   } else {
-    Serial.println("error en apertura de prueba.txt");	// texto de falla en apertura de archivo
+    Serial.println("error en apertura de prueba.txt");  // texto de falla en apertura de archivo
   }
 
-  archivo = SD.open("prueba.txt");		// apertura de archivo prueba.txt
+  archivo = SD.open("prueba.txt");    // apertura de archivo prueba.txt
   if (archivo) {
-    Serial.println("Contenido de prueba.txt:");	// texto en monitor serie
-    while (archivo.available()) {		// mientras exista contenido en el archivo
-      Serial.write(archivo.read());  		// lectura de a un caracter por vez
+    Serial.println("Contenido de prueba.txt:"); // texto en monitor serie
+    while (archivo.available()) {   // mientras exista contenido en el archivo
+      Serial.write(archivo.read());     // lectura de a un caracter por vez
     }
-    archivo.close();				// cierre de archivo
+    archivo.close();        // cierre de archivo
   } else {
     Serial.println("error en la apertura de prueba.txt");// texto de falla en apertura de archivo
   }
 }
 
-void loop() {			// funcion loop() obligatoria de declarar pero no utilizada
+void loop() {     // funcion loop() obligatoria de declarar pero no utilizada
   // nada por aqui
 }
